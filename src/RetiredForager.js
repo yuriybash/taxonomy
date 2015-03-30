937732ece4f8901deb0f365dbde98b92d689095f
Yuriy Bash <yuriybash@gmail.com>
2015-03-30 14:41:06 -0700

    completed Taxonomy

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,26 @@
 var RetiredForagerBee = function() {
+	ForagerBee.call(this);
+	this.age = 40;
+	this.job = "gamble";
+	this.canFly = false;
+	this.color = "grey";
+	//this.treasureChest = new ForagerBee().treasureChest;
 };
 
+
+
+
+RetiredForagerBee.prototype = Object.create(Grub.prototype)
+
+RetiredForagerBee.prototype.forage = function(){
+
+	console.log("TEST");
+	return ("I am too old, let me play cards instead")
+
+}
+
+RetiredForagerBee.prototype.gamble = function(treasure){
+	this.treasureChest.push(treasure);
+}
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
